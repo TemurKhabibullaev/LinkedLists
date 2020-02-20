1afea4671c593dd3a35147abf875079b5dc69a3d
TemurKhabibullaev <55756791+TemurKhabibullaev@users.noreply.github.com>
2020-02-20 14:34:25 -0500

    in process

---
+++ LinkedList.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, data = None, nxt = None):
+        self.data = data
+        self.nxt = nxt
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if self.head:
+            current = self.head
+            while current.nxt:
+                current = current.nxt
+            current.nxt = new_node
+        else:
+            self.head = new_node
+
+insta = Node(3)
+print(insta.data, insta.nxt)
+
